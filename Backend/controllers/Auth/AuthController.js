09552bb8a45c41a970ec8f650c166843dc57870b
Subhransu Sekhar Mishra <msubhransu1@gmail.com>
2026-03-12 14:21:24 +0530

    Change the import export module for backend

--- Backend/controllers/Auth/AuthController.js
+++ Backend/controllers/Auth/AuthController.js
@@ -1,1 +1,17 @@
-const express = require("express");
+import express from "express";
+export const doctorSignup = ()=>{
+    //Signup Logic 
+}
+
+export const doctorLogin = ()=>{
+    //Login Logic
+}
+
+export const patientSignup = ()=>{
+    //Signup Logic 
+}
+
+export const patientLogin = ()=>{
+    //Login Logic
+}
+
