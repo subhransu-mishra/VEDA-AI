f3bcee53c6a6df112f2f2afffd074ebadfa4896e
Abinash Kuanr <abinashkuanr677@gmail.com>
2026-03-13 21:00:31 +0530

    Random mes

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.listen(PORT,()=>{
     console.log("Running on PORT "+PORT);
     console.log("Backend2 Running from Tapu and MB")
+    console.log("Hello")
 })
 
 app.get("/", (req, res) => {
